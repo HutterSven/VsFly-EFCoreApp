1077bc0e5d23bb2e2c576fded6921997d5123c97
Jonathan Albrecht <jonathan.albrecht@hes-so.ch>
2021-06-13 11:01:44 +0200

    changed passengerID to key

--- EFCoreApp2021/Passenger.cs
+++ EFCoreApp2021/Passenger.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.ComponentModel.DataAnnotations;
 
 namespace EFCoreApp2021
 {
     public class Passenger:Person
     {
+        [Key]
         public int PassengerID { get; set; }
 
         // not finished
